84372689f4a9260b3be6ad5b91ea8ee100539a86
Jarvis (BallisticPain) Stubblefield <jarvis@vortexrevolutions.com>
2018-01-04 18:21:33 -0600

    First stab at an Android version of the programmatic volume control.

--- src/volume.android.ts
+++ src/volume.android.ts
@@ -1,5 +1,40 @@
+import * as app from 'tns-core-modules/application';
 import { Common } from './volume.common';
 
 export class Volume extends Common {
+  private _audioManager: android.media.AudioManager;
+  public AudioManager = android.media.AudioManager;
 
+  constructor() {
+    super();
+    this._audioManager = app.android.context.getSystemService(android.content.Context.AUDIO_SERVICE);
+  }
+
+  public mute(): void {
+    // Need the ADJUST_MUTE constant
+    // this._audioManager.adjustVolume(this.AudioManager.ADJUST_MUTE, this.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+    this._audioManager.adjustVolume(-100, this.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+  }
+
+  public unmute(): void {
+    // Need the ADJUST_UNMUTE constant
+    // this._audioManager.adjustVolume(this.AudioManager.ADJUST_UNMUTE, this.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+    this._audioManager.adjustVolume(100, this.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+  }
+
+  public volumeDown() {
+    this._audioManager.adjustVolume(this.AudioManager.ADJUST_LOWER, this.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+  }
+
+  public volumeUp() {
+    this._audioManager.adjustVolume(this.AudioManager.ADJUST_RAISE, this.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+  }
+
+  public getVolume(): number {
+    return this._audioManager.getStreamVolume(this.AudioManager.STREAM_MUSIC);
+  }
+
+  public setVolume(value: number) {
+    this._audioManager.setStreamVolume(this.AudioManager.STREAM_MUSIC, value, this.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+  }
 }
