7b726dddbc5c2b239b1d1ce634b449639ee94b9e
Jarvis (BallisticPain) Stubblefield <jarvis@vortexrevolutions.com>
2018-01-04 18:17:24 -0600

    Got iOS working.

--- src/volume.common.ts
+++ src/volume.common.ts
@@ -2,27 +2,16 @@ import { Observable } from 'tns-core-modules/data/observable';
 import * as app from 'tns-core-modules/application';
 import * as dialogs from 'tns-core-modules/ui/dialogs';
 
-export class Common extends Observable {
-  public message: string;
 
+export class Common extends Observable {
   constructor() {
     super();
-    this.message = Utils.SUCCESS_MSG();
   }
 
-  public greet() {
-    return "Hello, NS";
-  }
-}
-
-export class Utils {
-  public static SUCCESS_MSG(): string {
-    let msg = `Your plugin is working on ${app.android ? 'Android' : 'iOS'}.`;
-
-    setTimeout(() => {
-      dialogs.alert(`${msg} For real. It's really working :)`).then(() => console.log(`Dialog closed.`));
-    }, 2000);
-
-    return msg;
-  }
+  public getVolume() { }
+  public mute() { }
+  public setVolume(value: number) { }
+  public unmute() { }
+  public volumeDown() { }
+  public volumeUp() { }
 }
--- src/volume.ios.ts
+++ src/volume.ios.ts
@@ -1,5 +1,31 @@
 import { Common } from './volume.common';
 
 export class Volume extends Common {
+  constructor() {
+    super();
+  }
 
+  public mute(): void {
+    LxVolumeManager.mute();
+  }
+
+  public unmute(): void {
+    LxVolumeManager.unmute();
+  }
+
+  public volumeDown() {
+    this.setVolume(LxVolumeManager.currentVolume() - (1 / 16));
+  }
+
+  public volumeUp() {
+    this.setVolume(LxVolumeManager.currentVolume() + (1 / 16));
+  }
+
+  public getVolume(): number {
+    return LxVolumeManager.currentVolume();
+  }
+
+  public setVolume(value: number) {
+    LxVolumeManager.setVolume(value);
+  }
 }
