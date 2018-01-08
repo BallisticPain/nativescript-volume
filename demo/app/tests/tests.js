17fdf83c361308e6a06cd14af516b66858019aca
Jarvis (BallisticPain) Stubblefield <jarvis@vortexrevolutions.com>
2018-01-08 03:10:07 -0600

    Added tests to at least ensure my methods exist.

--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -10,3 +10,46 @@ describe("greet function", function() {
         expect(volume.greet()).toEqual("Hello, NS");
     });
 });
+
+describe("mute function", function() {
+    it("exists", function() {
+        expect(volume.mute).toBeDefined();
+    });
+});
+
+describe("unmute function", function() {
+    it("exists", function() {
+        expect(volume.unmute).toBeDefined();
+    });
+});
+
+describe("getVolume function", function() {
+    it("exists", function() {
+        expect(volume.getVolume).toBeDefined();
+    });
+
+    // This should actually return a number in an actual run?
+    it("returns null", function() {
+        expect(volume.getVolume()).toEqual(null);
+    });
+});
+
+describe("setVolume function", function() {
+    it("exists", function() {
+        expect(volume.setVolume).toBeDefined();
+    });
+
+    // How to check a number was passed in?
+});
+
+describe("volumeUp function", function() {
+    it("exists", function() {
+        expect(volume.volumeUp).toBeDefined();
+    });
+});
+
+describe("volumeDown function", function() {
+    it("exists", function() {
+        expect(volume.volumeDown).toBeDefined();
+    });
+});
