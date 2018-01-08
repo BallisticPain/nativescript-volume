01362faabf268ac366ae6795caf6fee899fc6017
Jarvis (BallisticPain) Stubblefield <jarvis@vortexrevolutions.com>
2018-01-08 03:25:11 -0600

    Updated version in lock. Fixed lint issue with typings/*.

--- src/typings/objc!LxVolumeManager.d.ts
+++ src/typings/objc!LxVolumeManager.d.ts
@@ -1,27 +1,27 @@
 
 declare class LxVolumeManager extends NSObject {
 
-	/**
-	 * Returns a new instance of the receiving class.
-	 */
-	static alloc(): LxVolumeManager; // inherited from NSObject
+  /**
+   * Returns a new instance of the receiving class.
+   */
+  static alloc(): LxVolumeManager; // inherited from NSObject
 
-	static beginObserveVolumeChange(volumeChangeCallBack: (p1: number) => void): void;
+  static beginObserveVolumeChange(volumeChangeCallBack: (p1: number) => void): void;
 
-	static currentVolume(): number;
+  static currentVolume(): number;
 
-	static mute(): void;
+  static mute(): void;
 
-	/**
-	 * Allocates a new instance of the receiving class, sends it an init message, and returns the initialized object.
-	 */
-	static new(): LxVolumeManager; // inherited from NSObject
+  /**
+   * Allocates a new instance of the receiving class, sends it an init message, and returns the initialized object.
+   */
+  static new(): LxVolumeManager; // inherited from NSObject
 
-	static setVolume(volume: number): void;
+  static setVolume(volume: number): void;
 
-	static stopObserveVolumeChange(): void;
+  static stopObserveVolumeChange(): void;
 
-	static unmute(): void;
+  static unmute(): void;
 }
 
 declare var LxVolumeManagerVersionNumber: number;
